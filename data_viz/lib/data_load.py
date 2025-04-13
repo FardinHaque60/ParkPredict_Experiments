983ca44fcb8eeab593765bc20c4ce1037e83e425
Fardin Haque <fardinhaque60@gmail.com>
2025-04-13 14:12:45 -0700

    added notebook to do data viz by date interval

---
+++ data_viz/lib/data_load.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+def load_data(data_folder_dir):
+    column_headers = ["timestamp", "garage name", "fullness"]
+    garages = ["South", "South Campus", "North", "West"]
+    garages = [s + " Garage" for s in garages]
+
+    all_data = pd.concat(
+        pd.read_csv(
+            os.path.join(data_folder_dir, file),
+            names=column_headers, 
+            header=None 
+        )
+        for file in os.listdir(data_folder_dir) if file.endswith(".csv")
+    )
+
+    # sort data in ascending order by timestamp
+    all_data['timestamp'] = pd.to_datetime(all_data['timestamp'])
+    all_data = all_data.sort_values(by='timestamp')
+    return all_data
